26aafa93e0ffce02e1fa03827f77d5666a3f29ae
限量版星回 <sivichan@hotmail.com>
2021-11-01 17:27:53 +0800

    提交Replacer和bracketsChecker

---
+++ BNSXMLReplacer.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace bns_ch_check
+{
+  public class BNSXMLReplacer
+  {
+    private string path;
+
+    public BNSXMLReplacer(string path)
+    {
+      this.path = path;
+    }
+
+    public void Replace(Dictionary<string, string> replaceMap)
+    {
+
+
+
+      XDocument doc = XDocument.Load(this.path);
+
+      XElement rootElement = doc.Element("table");
+
+      foreach (XElement textElement in rootElement.Elements("text"))
+      {
+        XElement originalElement = textElement.Element("original");
+
+        XElement replacementElement = textElement.Element("replacement");
+
+        XCData originalXCData = (XCData)originalElement.FirstNode;
+
+
+
+        if (replaceMap.ContainsKey(originalXCData.Value))
+        {
+          replacementElement.ReplaceNodes(new XCData(replaceMap[originalXCData.Value]));
+        }
+      }
+
+      doc.Save(this.path);
+    }
+  }
+}
